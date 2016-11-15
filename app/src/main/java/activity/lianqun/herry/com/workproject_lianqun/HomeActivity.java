a8c2df726ab97e7130e751ea4822964395de09a4
Remeber <275281462@qq.com>
2016-11-15 11:11:24 +0800

    冲突 添加

--- app/src/main/java/activity/lianqun/herry/com/workproject_lianqun/HomeActivity.java
+++ app/src/main/java/activity/lianqun/herry/com/workproject_lianqun/HomeActivity.java
@@ -2,10 +2,13 @@ package activity.lianqun.herry.com.workproject_lianqun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private String str;
+    private String str_www;
+    private Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
