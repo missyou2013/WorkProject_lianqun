9354acd62e6294144a95cafdbc7da2b485d72199
Herry <mylove2008me@hotmail.com>
2016-11-15 10:03:31 +0800

    11

---
+++ app/src/main/java/activity/lianqun/herry/com/workproject_lianqun/HomeActivity.java
@@ -0,0 +1,16 @@
+package activity.lianqun.herry.com.workproject_lianqun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private String str;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.ac_home);
+    }
+}
