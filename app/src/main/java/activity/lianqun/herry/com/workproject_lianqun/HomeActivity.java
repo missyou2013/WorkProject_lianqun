bec98842892b1f04e024dbe3c403989851ef1a8e
Herry <mylove2008me@hotmail.com>
2016-11-23 10:42:21 +0800

    123

--- app/src/main/java/activity/lianqun/herry/com/workproject_lianqun/HomeActivity.java
+++ app/src/main/java/activity/lianqun/herry/com/workproject_lianqun/HomeActivity.java
@@ -1,11 +1,18 @@
 package activity.lianqun.herry.com.workproject_lianqun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class HomeActivity extends AppCompatActivity {
 
+    @BindView(R.id.button2)
+    Button button2;
     private String str_wwwq;
     private Button btn;
 
@@ -14,11 +21,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//�������
-//123343243
-//qqqqqqqqqqq
         setContentView(R.layout.ac_home);
+        ButterKnife.bind(this);
 
+        PollingUtils.startPollingService(this, 5, PollingService.class, PollingService.ACTION);
+    }
 
+    @OnClick(R.id.button2)
+    public void onClick() {
+        System.out.println("Start polling service...");
+
+//        Toast.makeText(this, "aaaaaaaaaa", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Stop polling service
+        System.out.println("Stop polling service...");
+//        PollingUtils.stopPollingService(this, PollingService.class, PollingService.ACTION);
     }
 }
